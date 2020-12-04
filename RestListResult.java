d83ebb8004091bb3be08e5c33265e515b740fbf3
ju-hyeon123 <70941594+ju-hyeon123@users.noreply.github.com>
2020-12-05 02:36:48 +0900

    Create RestListResult.java

---
+++ RestListResult.java
@@ -0,0 +1,10 @@
+package com.example.mission3;
+
+import java.util.ArrayList;
+
+public class RestListResult {
+    String Type;
+    String showRange;
+
+    ArrayList<Rest> dailyList = new ArrayList<Rest>();
+}
