d75638a7d7264eb7348f90328ef04c2c9157d9ff
ju-hyeon123 <70941594+ju-hyeon123@users.noreply.github.com>
2020-12-05 02:34:51 +0900

    Create Rest.java

---
+++ Rest.java
@@ -0,0 +1,22 @@
+package com.example.mission3;
+
+public class Rest {
+    String rnum;
+    String rank;
+    String rankInten;
+    String rank01dAndNew;
+    String movieCd;
+    String movieNm;
+    String openDt;
+    String salesAmt;
+    String salesShare;
+    String salesInten;
+    String salesChange;
+    String salesAcc;
+    String audiCnt;
+    String audiInten;
+    String audiChange;
+    String audiAcc;
+    String scrnCnt;
+    String showCnt;
+}
