06aa3efcb7b81ced93434d9771aed75cf33e4990
ju-hyeon123 <70941594+ju-hyeon123@users.noreply.github.com>
2020-12-05 02:33:38 +0900

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.mission3;
+
+import android.graphics.Movie;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView textView;
+
+    static RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        editText = findViewById(R.id.xeditText);
+        textView = findViewById(R.id.xtextView);
+
+        Button button = findViewById(R.id.xbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeRequest();
+            }
+        });
+
+        if (requestQueue == null)
+        {
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+    }
+
+    public void makeRequest() {
+        String url = editText.getText().toString();
+
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        println("결과 -> " + response);
+                        processResponse(response);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        println("오류 -> " + error.getMessage());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                return super.getParams();
+            }
+        };
+
+        request.setShouldCache(false);
+        requestQueue.add(request);
+        println("전송");
+    }
+
+    public void println(String data) { textView.append(data + "\n"); }
+
+    public void processResponse(String response)
+    {
+        Gson gson = new Gson();
+        RestList restList = gson.fromJson(response, RestList.class);
+        println("쉼터 수 : "+ restList.areaResult.dailyList.size());
+    }
+
+}
