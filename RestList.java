c515a1a16fadbc6f45f5c9457c61b99909052976
ju-hyeon123 <70941594+ju-hyeon123@users.noreply.github.com>
2020-12-05 02:36:20 +0900

    Update RestList.java

--- RestList.java
+++ RestList.java
@@ -3,16 +3,3 @@ package com.example.mission3;
 public class RestList {
     RestListResult areaResult;
 }
-
-6. [RestListResult.java]
-
-package com.example.mission3;
-
-import java.util.ArrayList;
-
-public class RestListResult {
-    String Type;
-    String showRange;
-
-    ArrayList<Rest> dailyList = new ArrayList<Rest>();
-}
